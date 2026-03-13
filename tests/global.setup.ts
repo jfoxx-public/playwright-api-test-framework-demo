adce19114adaecfefe5dcccf37664ba064ac2f88
jfoxx.public <jfoxx.public@gmail.com>
2026-03-13 13:42:39 -0700

    chore: display active environment during global setup

--- tests/global.setup.ts
+++ tests/global.setup.ts
@@ -1,10 +1,14 @@
 import { test as teardown } from '@playwright/test';
 import Logger from '@/utils/logger';
-import { currentEnv } from '@/api/data/users';
+import { ENV, BASE_URLS } from '@/config/env';  //>import { currentEnv } from '@/api/data/users';
+
 
 teardown('Creating database', async ()=>{
   Logger.info('Before all -> Starting global setup...');
-  Logger.info(`Setting up environment ("${currentEnv.toUpperCase()}")`);
+  Logger.info(`*********************************`);
+  Logger.info(` - Environment: ${ENV}`);
+  Logger.info(` - Base URL: ${BASE_URLS[ENV]}`);
+  Logger.info(`*********************************`);
   Logger.info('await resetDatabase();  // clean the database');
   Logger.info('await seedUsers();      // generate default users for the environment');
   Logger.info('Global setup completed.');
